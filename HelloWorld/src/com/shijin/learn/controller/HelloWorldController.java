2dd6a6f09bdf5c79675067331c74d88e24aa11de
jeri <shijin2008@gmail.com>
2017-02-05 15:51:43 +0800

    Spring MVC init

---
+++ HelloWorld/src/com/shijin/learn/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.shijin.learn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+
+  @RequestMapping("/welcome")
+  public ModelAndView helloWorld() {
+    String message = "World";
+    return new ModelAndView("welcome", "message", message);
+  }
+}
